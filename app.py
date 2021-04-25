d506dc5e56ee90fd319e5c2051063366fe916b3a
Africa Freedom <66422095+Jataz@users.noreply.github.com>
2021-04-26 00:18:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,95 +1,32 @@
-from flask import Flask, request, session, redirect
-from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
-import requests
-import random, os
-import sqlite3
-from datetime import datetime
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
 
-app = Flask(__name__)
-app.secret_key = "this-is-a-secret-key"
+from flask_migrate import Migrate
+from os import environ
+from sys import exit
+from decouple import config
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+from config import config_dict
+from app import create_app, db
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming messages """
+# WARNING: Don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=True)
+
+# The configuration
+get_config_mode = 'Debug' if DEBUG else 'Production'
+
+try:
     
-    msg = request.form.get('Body').lower()
-    number = request.form.get('From').replace("whatsapp:", " ")
-    date = datetime.today()
-    resp = MessagingResponse()
-        
-    
-   
-    if "taxi" in msg:
-        connection = sqlite3.connect('WhatsappTaxiOrg.db')
-        connection.execute("INSERT INTO customers (phone_number, date_and_time) VALUES (?, ?)", (number, date ))
-        connection.commit()
-        connection.close()
-        session["stage"] = "stage2" 
-        resp.message("🚕🚖🇿🇼🚕🚖\n*Welcome to whatsapp taxi booking. Type in your option:*\n\n1.Book a taxi\n2.About Us\n🚕🚖🇿🇼🚕🚖\n\n")
-        resp.message(number)
+    # Load the configuration using the default values 
+    app_config = config_dict[get_config_mode.capitalize()]
 
-    elif "stage" in session:
-        stage = session["stage"] 
-        if stage == "stage2" and msg =="1":
-            session["stage"] = "booking"
-            resp.message("*🏙️🌆Enter your current city*")
+except KeyError:
+    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
-        elif stage == "stage2" and msg =="2":
-            session["stage"] = "stage2"
-            resp.message("*🇿🇼🚖ABOUT US🚖🇿🇼*\n\nXXXXXXXXXXXXX")
-
-
-        elif stage == "booking" and msg =="harare":
-            session["stage"] = "stage3"
-            resp.message("*Share your live location*📍")
-
-
-        elif stage == "booking" and msg != "harare":
-            session["stage"] = "stage3"
-            resp.message(f"*😕🙇🏽Sorry, currently they are no taxis available in {msg}.*\nType the word *Taxi* to go back to menu.")
-
-
-        elif stage == "stage3":
-            if 'Latitude' in request.values.keys() and 'Longitude' in request.values.keys():
-                session["stage"] = "user_location" 
-        
-                message_latitude = request.values.get('Latitude', None),
-                message_longitude = request.values.get('Longitude', None),
-                message_address = request.values.get('Address', None)
-
-    
-            #resp.message("Your latitude is :" +str(message_latitude) +"\nYour Longitude is :"+ str(message_longitude)+" \n Your address is :" + str(message_address))
-                PersistentAction= f'geo:Your Latitude is {message_latitude}\nYour Longitude is {message_longitude}\n{message_address}'
-                resp.message(PersistentAction)
-                
-
-            elif 'Latitude' not in request.values.keys() and 'Longitude' not in request.values.keys():
-                session["stage"] = "user_location" 
-                resp.message("❌Invalid location❌\nType the word *Taxi* to go back to menu")
-
-
-        elif stage == "stage2" and msg != "1" or "2":
-            session["stage"] = "stage2"
-            resp.message("❌Invalid input❌\nType the word *Taxi* to get started.")
-
-    else:
-        resp.message("❌Invalid input❌\nType the word *Taxi* to get started.")
-
-    return str(resp)
-
-
+app = create_app( app_config ) 
+Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
-
-
+    app.run()
